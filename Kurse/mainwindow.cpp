abfce5d1e1bf49ae35698b75bb9fc68de5a3880c
Max Myasnenko <f10u@yandex.ru>
2019-12-16 00:12:39 +0700

    Первый залив

---
+++ Kurse/mainwindow.cpp
@@ -0,0 +1,86 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPushButton>
+#include <QMenu>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //Выпадающий список для первой кнопки
+    QMenu*       pm = new QMenu(ui->pushButton);
+    pm->addAction("Готовимся к путешествию", this, SLOT(first_sub_menu()));
+    pm->addAction("Путешествуем с детьми", this, SLOT(second_sub_menu()));
+    pm->addAction("Путешествуем с животными", this, SLOT(third_sub_menu()));
+    pm->addAction("В аэропорте и самолёте", this, SLOT(fourth_sub_menu()));
+    pm->addAction("Безопастность", this, SLOT(fifth_sub_menu()));
+    ui->pushButton->setMenu(pm);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+//Реализация нажатия на первое подменю
+void MainWindow::first_sub_menu()
+{
+    ui->textEdit->setText("Подменю Готовимся к путешествию");
+}
+
+//Реализация нажатия на второе подменю
+void MainWindow::second_sub_menu()
+{
+    ui->textEdit->setText("Подменю Путешествие с детьми");
+}
+
+//Реализация нажатия на третье подменю
+void MainWindow::third_sub_menu()
+{
+    ui->textEdit->setText("Подменю Путешествуем с животными");
+}
+
+//Реализация нажатия на четвёртое подменю
+void MainWindow::fourth_sub_menu()
+{
+    ui->textEdit->setText("В аэропорте и самолёте");
+}
+
+//Реализация нажатия на четвёртое подменю
+void MainWindow::fifth_sub_menu()
+{
+    ui->textEdit->setText("Безопастность");
+}
+
+//Реализация нажатия на кнопку "Список с собой"
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->textEdit->setText("Список с собой");
+}
+
+//Реализация нажатия на кнопку "Хочу посетить"
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->textEdit->setText("Хочу посетить");
+    QPushButton *kek = new QPushButton(ui->widget);
+    kek->setText("dasdas");
+    kek->show();
+
+}
+
+//Реализация нажатия на кнопку "Посетил"
+void MainWindow::on_pushButton_4_clicked()
+{
+    ui->textEdit->setText("Посетил");
+    delete(ui->widget);
+}
+
+//Реализация нажатия на кнопку "Настройки"
+void MainWindow::on_pushButton_5_clicked()
+{
+    ui->textEdit->setText("Настройки");
+}
---
+++ Kurse/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <iostream>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    //Слоты для подменю "Необходимая информация"
+    void first_sub_menu();
+    void second_sub_menu();
+    void third_sub_menu();
+    void fourth_sub_menu();
+    void fifth_sub_menu();
+
+    //Слоты для кнопок
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
+
